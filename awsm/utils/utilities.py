9c5d4664ccdb41cc606c96c09216578157e84386
Micah Sandusky <micah.sandusky@ars.usda.gov>
2018-03-30 12:40:49 -0600

    Code to read the git version

---
+++ awsm/utils/utilities.py
@@ -0,0 +1,17 @@
+def getgitinfo():
+    """gitignored file that contains specific AWSM version and path
+
+    Input:
+        - none
+    Output:
+        - path to base AWSM directory
+        - git version from 'git describe'
+    """
+    # return git describe if in git tracked SMRF
+    if len(__gitVersion__) > 1:
+        return __gitVersion__
+
+    # return overarching version if not in git tracked SMRF
+    else:
+        version = 'v'+__version__
+        return version

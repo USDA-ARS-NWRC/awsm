579c90c4ae0714962dec480aa18c02ba2999cdf5
Micah Sandusky <micah.sandusky@ars.usda.gov>
2018-03-30 12:32:12 -0600

    code to fetch most recent git tag when installing awsm and write it to a file

--- setup.py
+++ setup.py
@@ -2,8 +2,47 @@
 # -*- coding: utf-8 -*-
 
 """The setup script."""
-
 from setuptools import setup, find_packages
+import os
+import subprocess
+from subprocess import check_output, PIPE
+
+#Grab and write the gitVersion from 'git describe'.
+gitVersion = ''
+gitPath = ''
+
+# get git describe if in git repository
+print('Fetching most recent git tags')
+if os.path.exists('./.git'):
+	try:
+		# if we are in a git repo, fetch most recent tags
+		check_output(["git fetch --tags"], shell=True)
+	except Exception as e:
+		print(e)
+		print('Unable to fetch most recent tags')
+
+	try:
+		ls_proc = check_output(["git describe --tags"], shell=True, universal_newlines=True)
+		gitVersion = ls_proc
+		print('Checking most recent version')
+	except Exception as e:
+		print('Unable to get git tag and hash')
+# if not in git repo
+else:
+	print('Not in git repository')
+	gitVersion = ''
+
+# get current working directory to define git path
+gitPath = os.getcwd()
+
+# git untracked file to store version and path
+fname = os.path.abspath(os.path.expanduser('./awsm/utils/gitinfo.py'))
+
+with open(fname,'w') as f:
+	nchars = len(gitVersion) - 1
+	f.write("__gitPath__='{0}'\n".format(gitPath))
+	f.write("__gitVersion__='{0}'\n".format(gitVersion[:nchars]))
+	f.close()
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
@@ -35,7 +74,8 @@ setup(
 			  'awsm.convertFiles',
 			  'awsm.interface',
 			  'awsm.framework',
-              'awsm.knn'
+              'awsm.knn',
+			  'awsm.utils'
 			  ],
 
 

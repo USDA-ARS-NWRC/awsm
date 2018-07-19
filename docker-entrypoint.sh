29d5786353a2de3a38019c8317860ea9b5df6bd4
Micah Sandusky <micah.sandusky@ars.usda.gov>
2018-07-19 17:53:31 +0000

    adding test case to docker entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,13 @@ if [ $# -eq 0 ]; then
     echo "Run AWSM docker test"
     exec $awsm /code/awsm/test_data/RME_run/docker_pysnobal.ini
 
+elif [ "$1" = "test" ]; then
+    echo "Run AWSM docker test"
+    cd /code/awsm
+    coverage run --source awsm setup.py test
+
+    coverage report --fail-under=70
+
 elif [ ! -x "$1" ]; then
     echo "Run AWSM with config file"
     exec $awsm "$1"

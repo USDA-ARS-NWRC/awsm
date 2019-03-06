993bd6aa072d33c956b506817123bc7d7b693dc6
Micah Sandusky <micah.sandusky@ars.usda.gov>
2019-03-06 22:07:05 +0000

    Bump version: 0.9.4 → 0.9.5

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ test_requirements = [
 
 setup(
     name='awsm',
-    version='0.9.4',
+    version='0.9.5',
     description="Automated Water Supply Model",
     # long_description=readme + '\n\n' + history,
     author="Micah Sandusky",

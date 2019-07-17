dcf18f64b037c66dd9c76d5c24d5e62e764ec4ff
Mark Robertson <markrobertson@u.boisestate.edu>
2019-07-17 16:58:41 +0000

    Bump version: 0.9.20 → 0.9.21

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ test_requirements = [
 
 setup(
     name='awsm',
-    version='0.9.20',
+    version='0.9.21',
     description="Automated Water Supply Model",
     # long_description=readme + '\n\n' + history,
     author="Micah Sandusky",

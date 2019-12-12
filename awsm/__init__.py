c65bf3ee30069e038d7cbf1daae0946959c822cb
Mark Robertson <markrobertson@u.boisestate.edu>
2019-12-12 15:08:03 +0000

    Bump version: 0.9.33 → 0.9.34

--- awsm/__init__.py
+++ awsm/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Micah Sandusky"""
 __email__ = 'micah.sandusky@ars.usda.gov'
-__version__ = '0.9.33'
+__version__ = '0.9.34'
 import matplotlib
 matplotlib.use('Agg')
 import os
--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ test_requirements = [
 
 setup(
     name='awsm',
-    version='0.9.33',
+    version='0.9.34',
     description="Automated Water Supply Model",
     # long_description=readme + '\n\n' + history,
     author="Micah Sandusky",

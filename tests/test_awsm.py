6d11f8845852737d86b105a1a402b9464c8638f3
Scott Havens <scotthavens42@gmail.com>
2020-09-15 13:12:19 -0600

    added tests/basin with Lakes and RME, updated base test case with that from SMRF

--- tests/test_awsm.py
+++ tests/test_awsm.py
@@ -1,48 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-test_awsm
-----------------------------------
-
-Tests for an entire awsm run. The AWSM integration run!
-"""
-
 import os
-import shutil
-import unittest
-
-import numpy.testing as npt
-from netCDF4 import Dataset
 
 from awsm.framework.framework import run_awsm
 from tests.awsm_test_case import AWSMTestCase
-
-
-def compare_image(variable, test_image, gold_image):
-    """
-    Compares variables between the two netCDF images and determines
-    if they are the same.
-
-    Args:
-        variable: Compared variable name
-        test_image: Absolute path to tested image
-        gold_image: Absolute path to gold image
-    """
-
-    d1 = Dataset(gold_image, 'r')
-    gold_data = d1.variables[variable][:]
-    d1.close()
-
-    d2 = Dataset(test_image, 'r')
-    test_data = d2.variables[variable][:]
-    d2.close()
-
-    npt.assert_array_equal(
-        test_data,
-        gold_data,
-        err_msg=f"Variable ${variable} did not match with gold image"
-    )
 
 
 class TestStandardRME(AWSMTestCase):
@@ -53,142 +12,74 @@ class TestStandardRME(AWSMTestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.run_dir = os.path.abspath(os.path.join(cls.test_dir, 'RME'))
 
-        cls.gold_path = os.path.join(cls.run_dir, 'gold')
-        cls.gold_em = os.path.join(cls.gold_path, 'em.nc')
-        cls.gold_snow = os.path.join(cls.gold_path, 'snow.nc')
+        cls.gold_dir = cls.basin_dir.joinpath('gold')
 
-        cls.output = os.path.join(
-            cls.run_dir,
+        cls.gold_em = os.path.join(cls.gold_dir, 'em.nc')
+        cls.gold_snow = os.path.join(cls.gold_dir, 'snow.nc')
+
+        cls.output_path = cls.basin_dir.joinpath(
             'output/rme/devel/wy1986/rme_test/runs/run3337_3344'
         )
-        cls.output_em = os.path.join(cls.output, 'em.nc')
-        cls.output_snow = os.path.join(cls.output, 'snow.nc')
 
-        # Remove any potential files to ensure fresh run
-        if os.path.isdir(cls.output):
-            shutil.rmtree(cls.output)
-
-        config = os.path.join(cls.run_dir, 'config.ini')
+        config = os.path.join(cls.basin_dir, 'config.ini')
 
         cls.cache_run = True
         run_awsm(config)
 
     def test_thickness(self):
-        """
-        Check the simulated thickness is the same as the gold file
-        """
-        compare_image("thickness", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'thickness')
 
     def test_snow_density(self):
-        """
-        Check the simulated snow density is the same as the gold file
-        """
-        compare_image("snow_density", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'snow_density')
 
     def test_specific_mass(self):
-        """
-        Check the simulated specific mass is the same as the gold file
-        """
-        compare_image("specific_mass", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'specific_mass')
 
     def test_liquid_water(self):
-        """
-        Check the simulated liquid water is the same as the gold file
-        """
-        compare_image("liquid_water", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'liquid_water')
 
     def test_temp_surf(self):
-        """
-        Check the simulated temp surf is the same as the gold file
-        """
-        compare_image("temp_surf", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'temp_surf')
 
     def test_temp_lower(self):
-        """
-        Check the simulated temp lower is the same as the gold file
-        """
-        compare_image("temp_lower", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'temp_lower')
 
     def test_temp_snowcover(self):
-        """
-        Check the simulated temp snowcover is the same as the gold file
-        """
-        compare_image("temp_snowcover", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'temp_snowcover')
 
     def test_thickness_lower(self):
-        """
-        Check the simulated thickness lower is the same as the gold file
-        """
-        compare_image("thickness_lower", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'thickness_lower')
 
     def test_water_saturation(self):
-        """
-        Check the simulated water saturation is the same as the gold file
-        """
-        compare_image("water_saturation", self.output_snow, self.gold_snow)
+        self.compare_netcdf_files('snow.nc', 'water_saturation')
 
     def test_net_rad(self):
-        """
-        Check the simulated net rad is the same as the gold file
-        """
-        compare_image("net_rad", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'net_rad')
 
     def test_sensible_heat(self):
-        """
-        Check the simulated sensible heat is the same as the gold file
-        """
-        compare_image("sensible_heat", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'sensible_heat')
 
     def test_latent_heat(self):
-        """
-        Check the simulated latent heat is the same as the gold file
-        """
-        compare_image("latent_heat", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'latent_heat')
 
     def test_snow_soil(self):
-        """
-        Check the simulated snow soil is the same as the gold file
-        """
-        compare_image("snow_soil", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'snow_soil')
 
     def test_precip_advected(self):
-        """
-        Check the simulated precip advected is the same as the gold file
-        """
-        compare_image("precip_advected", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'precip_advected')
 
     def test_sum_EB(self):
-        """
-        Check the simulated sum EB is the same as the gold file
-        """
-        compare_image("sum_EB", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'sum_EB')
 
     def test_evaporation(self):
-        """
-        Check the simulated evaporation is the same as the gold file
-        """
-        compare_image("evaporation", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'evaporation')
 
     def test_snowmelt(self):
-        """
-        Check the simulated snowmelt is the same as the gold file
-        """
-        compare_image("snowmelt", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'snowmelt')
 
     def test_SWI(self):
-        """
-        Check the simulated SWI is the same as the gold file
-        """
-        compare_image("SWI", self.output_em, self.gold_em)
+        self.compare_netcdf_files('em.nc', 'SWI')
 
     def test_cold_content(self):
-        """
-        Check the simulated cold content is the same as the gold file
-        """
-        compare_image("cold_content", self.output_em, self.gold_em)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.compare_netcdf_files('em.nc', 'cold_content')
